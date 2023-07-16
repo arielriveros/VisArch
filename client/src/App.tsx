39541af94f6710cbbb725894aeb326b20cb02db8
Ariel <ariel.riveros@ing.uchile.cl>
2023-07-16 02:13:05 -0400

    feat (client): redirect to home on logout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import Navbar from './components/navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App(): JSX.Element {
+
+  const { user } = useAuthContext();
 
   return (
     <div className="App">
@@ -12,7 +15,7 @@ function App(): JSX.Element {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects" element={ user ? <Projects /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </BrowserRouter>
