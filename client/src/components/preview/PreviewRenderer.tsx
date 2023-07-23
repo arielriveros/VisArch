619308c92fb8317dbc69a2179024e434e50944af
Ariel <ariel.riveros@ing.uchile.cl>
2023-07-23 01:19:01 -0400

    feat (client): orbit controls on preview

--- client/src/components/preview/PreviewRenderer.tsx
+++ client/src/components/preview/PreviewRenderer.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber';
+import React, { useEffect } from 'react'
+import { Canvas, useThree } from '@react-three/fiber';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+function CameraController() {
+    const { camera, gl } = useThree();
+    useEffect(() => {
+        const controls = new OrbitControls(camera, gl.domElement);  
+        controls.enablePan = false;
+        controls.enableZoom = false;
+
+        return () => {
+            controls.dispose();
+        };
+    }, [camera, gl]);
+    
+    return null;
+}
 
 export default function PreviewRenderer({ children }: { children: React.ReactNode }) {
-  return (
-    <Canvas camera={{position: [0, 1, 1]}}>
-        <color attach="background" args={['black']} />
-        <ambientLight />
-        { children }
-    </Canvas>
+
+    return (
+        <Canvas camera={{position: [0, 1, 1]}}>
+            <CameraController />
+            <color attach="background" args={['black']} />
+            <ambientLight />
+            { children }
+        </Canvas>
   )
 }
