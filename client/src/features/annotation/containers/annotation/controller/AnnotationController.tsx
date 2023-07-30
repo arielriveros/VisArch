77704e2422558abf24327b7a6135abe2fe950424
Ariel <ariel.riveros@ing.uchile.cl>
2023-07-30 06:10:59 -0400

    feat (client): unwrap object in controller

--- client/src/features/annotation/containers/annotation/controller/AnnotationController.tsx
+++ client/src/features/annotation/containers/annotation/controller/AnnotationController.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
 import { Canvas, useThree } from '@react-three/fiber';
 import { IntersectionPayload } from '../manager/AnnotationManager';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { useEffect } from 'react';
 import { useProxyMeshContext } from '../../../hooks/useProxyMesh';
+import { BufferAttribute, BufferGeometry, Group, Material, Mesh, NormalBufferAttributes } from 'three';
+import { radialUnwrap } from '../../../utils/radialUnwrap';
 import './AnnotationController.css';
 
 function CameraController() {
@@ -12,8 +14,6 @@ function CameraController() {
         controls.enablePan = true;
         controls.enableZoom = true;
 		controls.enableRotate = false;
-		controls.minZoom = 0.1;
-		controls.maxZoom = 10;
 
         return () => {
             controls.dispose();
@@ -24,7 +24,7 @@ function CameraController() {
 }
 
 function SelectIndex(props: {selectIndex: (index: IntersectionPayload | null) => void}) {
-    const { raycaster, scene } = useThree();
+    const { raycaster, scene, camera } = useThree();
     let isThrottled = false;
 
     const handleMouseMove = (e: MouseEvent) => {
@@ -36,13 +36,13 @@ function SelectIndex(props: {selectIndex: (index: IntersectionPayload | null) =>
         // Perform raycasting and intersection calculations
         const intersects = raycaster.intersectObjects(scene.children, true);
         if (intersects.length > 0) {
-        const intersection: IntersectionPayload = {
-            face: intersects[0].face ? intersects[0].face : null,
-            faceIndex: intersects[0].faceIndex !== undefined ? intersects[0].faceIndex : null,
-        };
-        props.selectIndex(intersection);
+            const intersection: IntersectionPayload = {
+                face: intersects[0].face ? intersects[0].face : null,
+                faceIndex: intersects[0].faceIndex !== undefined ? intersects[0].faceIndex : null,
+            };
+            props.selectIndex(intersection);
         }
-
+    
         // Set a timeout to reset the throttling flag
         setTimeout(() => {
         isThrottled = false;
@@ -50,12 +50,10 @@ function SelectIndex(props: {selectIndex: (index: IntersectionPayload | null) =>
     };
 
     useEffect(() => {
-        // Attach the event listener on mount
         window.addEventListener('mousemove', handleMouseMove);
 
         return () => {
-        // Remove the event listener on unmount
-        window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mousemove', handleMouseMove);
         };
     }, []);
 
@@ -70,6 +68,40 @@ export default function AnnotationController(props: AnnotationViewerProps) {
 
     const { proxyMesh } = useProxyMeshContext();
 	const { selectIndexHandler } = props;
+    const [unwrappedMesh , setUnwrappedMesh] = useState<Group | null>(null);
+    const [unwrapAxis, setUnwrapAxis] = useState<'x' | 'y' | 'z'>('y');
+
+    const unwrapMesh = () => {
+        const material: Material | undefined = proxyMesh?.material?.clone();            
+        if(material) material.side = 0;
+        const unwrappedMesh = new Mesh(proxyMesh?.geometry?.clone() as BufferGeometry<NormalBufferAttributes>, material);
+        const group = new Group();
+        group.add(unwrappedMesh);
+        
+        if (proxyMesh?.geometry) {
+            const unwrappedPositions = radialUnwrap(Array.from(proxyMesh.geometry.attributes.position.array), unwrapAxis);
+            const positionsBufferAttribute = new BufferAttribute(new Float32Array(unwrappedPositions), 3);
+            unwrappedMesh.geometry?.setAttribute('position', positionsBufferAttribute);
+            unwrappedMesh.geometry?.rotateX(Math.PI / 2);
+            unwrappedMesh.geometry?.rotateY(Math.PI / 2);
+
+            unwrappedMesh.geometry?.translate(0, 2, 0);
+
+            
+            unwrappedMesh.geometry?.normalizeNormals();
+            unwrappedMesh.geometry?.computeVertexNormals();
+            unwrappedMesh.geometry?.computeTangents();
+            unwrappedMesh.geometry?.computeBoundingBox();
+            unwrappedMesh.geometry?.computeBoundingSphere();
+            
+        } 
+        setUnwrappedMesh(group);
+    };
+
+    useEffect(() => {
+        unwrapMesh();
+    }, [proxyMesh, unwrapAxis]);
+
 
 	return (
 		<div className="annotation-viewer-container">
@@ -77,9 +109,10 @@ export default function AnnotationController(props: AnnotationViewerProps) {
 				<CameraController />
                 <SelectIndex selectIndex={(index: IntersectionPayload | null)=>selectIndexHandler(index)} />
 				<ambientLight />
+                {/* <axesHelper args={[5]} /> */}
 				<color attach="background" args={['gray']} />
 				<pointLight position={[10, 10, 10]} />
-				{proxyMesh?.geometry && <mesh geometry={proxyMesh.geometry} material={proxyMesh.material} />}
+                {unwrappedMesh && <primitive object={unwrappedMesh}  />}
 			</Canvas>
 		</div>
 	);
---
+++ client/src/features/annotation/utils/radialUnwrap.ts
@@ -0,0 +1,27 @@
+export function radialUnwrap(vertices: number[], axis: 'x' | 'y' | 'z' = 'y') {
+    const unwrappedPositions = [];
+
+    for (let i = 0; i < vertices.length; i += 3) {
+        let [x, y, z] = vertices.slice(i, i + 3);
+
+        switch (axis) {
+			case 'x':
+				[x, y, z] = [y, z, x];
+				break;
+			case 'y':
+				[x, y, z] = [x, y, z];
+				break;
+			case 'z':
+				[x, y, z] = [z, x, y];
+				break;
+        }
+        
+        const r = Math.sqrt(x * x + y * y + z * z);
+        const theta = Math.atan2(x, z);
+        const phi = Math.acos(y / r);
+
+        unwrappedPositions.push(r, theta, phi);
+    }
+
+    return unwrappedPositions;
+};
