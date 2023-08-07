32727e221149d3f3fc54f4f71e1aa3fd9d748b99
Ariel <ariel.riveros@ing.uchile.cl>
2023-08-07 13:52:05 -0400

    refactor (client): unwrapped mesh is exclusive from group, abstract highlightMesh function

--- client/src/features/annotation/containers/annotation/controller/AnnotationController.tsx
+++ client/src/features/annotation/containers/annotation/controller/AnnotationController.tsx
@@ -1,16 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { BufferAttribute, ColorRepresentation, Group, Material, Mesh, MeshBasicMaterial } from 'three';
 import { IntersectionPayload } from '../manager/AnnotationManager';
 import { useProxyMeshContext } from '../../../hooks/useProxyMesh';
-import { BufferAttribute, BufferGeometry, Group, Material, Mesh, MeshBasicMaterial, NormalBufferAttributes } from 'three';
+import { useTaskContext } from '../../../hooks/useTask';
 import { radialUnwrap } from '../../../utils/radialUnwrap';
 import { flattenAxis } from '../../../utils/flattenAxis';
+import { highlightIndices } from '../../../utils/highlightIndices';
 import CameraController from './CameraController';
 import HoverIndex from './HoverIndex';
 import LassoSelector from './LassoSelector';
-import './AnnotationController.css';
 import Confirmation from '../../../components/confirmation/Confirmation';
-import { useTaskContext } from '../../../hooks/useTask';
+import './AnnotationController.css';
 
 interface AnnotationViewerProps {
     hoverIndexHandler: (index: IntersectionPayload | null) => void;
@@ -18,55 +19,78 @@ interface AnnotationViewerProps {
 }
 
 export default function AnnotationController(props: AnnotationViewerProps) {
-    const { selectedArchetype: archetype } = useTaskContext();
 	const { hoverIndexHandler, selectIndicesHandler } = props;
+    const { selectedArchetype: archetype } = useTaskContext();
     const { proxyGeometry, proxyMaterial } = useProxyMeshContext();
-    const [unwrappedMesh , setUnwrappedMesh] = useState<Group | null>(null);
-    const [highlightMesh , setHighlightMesh] = useState<Mesh>(new Mesh());
-    const [unwrapAxis, setUnwrapAxis] = useState<'x' | 'y' | 'z'>('y');
+    const [unwrappedMesh, setUnwrappedMesh] = useState<Mesh>(new Mesh());
+    const [highlightMesh, setHighlightMesh] = useState<Mesh>(new Mesh());
     const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
     const [tempIndices, setTempIndices] = useState<number[]>([]);
+    
+    const groupRef = useRef<Group | null>(null);
 
-    const unwrapMesh = () => {
-        const material: Material | undefined = proxyMaterial?.clone();            
-        if (material) material.side = 0;
-        const unwrappedMesh = new Mesh(proxyGeometry?.clone() as BufferGeometry<NormalBufferAttributes>, material);
-        const group = new Group();
-        group.add(unwrappedMesh);
-        
-        if (proxyGeometry) {
-            const unwrappedPositionsNotFlattened = radialUnwrap(Array.from(proxyGeometry.attributes.position.array), unwrapAxis);
-            const unwrappedPositions = flattenAxis(unwrappedPositionsNotFlattened, "x", 0.05);
+    const init = async () => {
+        groupRef.current = new Group();
+        const unwrappedProxyMesh = await unwrapMesh('y');
+        if (!unwrappedProxyMesh) return;
+
+        setUnwrappedMesh(unwrappedProxyMesh);
+        groupRef.current?.add(unwrappedProxyMesh);
+
+        const highlightMesh = createHighlightMesh(unwrappedProxyMesh, 'red');
+        setHighlightMesh(highlightMesh);
+        groupRef.current?.add(highlightMesh);
+    }
+
+    const unwrapMesh = async (unwrapAxis: 'x' | 'y' | 'z') => {
+        return new Promise<Mesh | null>((resolve) => {
+            if (!proxyGeometry || !proxyMaterial) {
+                resolve(null);
+                return;
+            }
+
+            const material: Material = proxyMaterial.clone();
+            if (material) material.side = 0;
+
+            const unwrappedProxyMesh = new Mesh();
+            unwrappedProxyMesh.geometry = proxyGeometry.clone();
+            unwrappedProxyMesh.material = material;
+            const unwrappedPositionsNotFlattened = radialUnwrap(
+                Array.from(proxyGeometry.attributes.position.array),
+                unwrapAxis
+            );
+            const unwrappedPositions = flattenAxis(unwrappedPositionsNotFlattened, 'x', 0.05);
             const positionsBufferAttribute = new BufferAttribute(new Float32Array(unwrappedPositions), 3);
 
-            unwrappedMesh.geometry.setAttribute('position', positionsBufferAttribute);
+            unwrappedProxyMesh.geometry.setAttribute('position', positionsBufferAttribute);
 
-            unwrappedMesh.geometry.rotateX(Math.PI / 2);
-            unwrappedMesh.geometry.rotateY(-Math.PI / 2);
-            unwrappedMesh.geometry.translate(0, 2, 0);
-            
-            unwrappedMesh.geometry.computeBoundsTree();
-            unwrappedMesh.geometry.computeVertexNormals();
-            unwrappedMesh.geometry.computeTangents();
+            unwrappedProxyMesh.geometry.rotateX(Math.PI / 2);
+            unwrappedProxyMesh.geometry.rotateY(-Math.PI / 2);
+            unwrappedProxyMesh.geometry.translate(0, 2, 0);
 
-            const highlightMesh = new Mesh();
-            highlightMesh.geometry = unwrappedMesh.geometry.clone();
-            highlightMesh.geometry.drawRange.count = 0;
-            highlightMesh.material = new MeshBasicMaterial({
-                opacity: 0.5,
-                color: 0xff9800,
-                depthWrite: false,
-                transparent: true,
-                
-            });
-            highlightMesh.renderOrder = 1;
-            highlightMesh.geometry.computeBoundsTree();
-            
-            setHighlightMesh(highlightMesh);
-            group.add(highlightMesh);
-        } 
-        setUnwrappedMesh(group);
+            unwrappedProxyMesh.geometry.computeBoundsTree();
+            unwrappedProxyMesh.geometry.computeVertexNormals();
+            unwrappedProxyMesh.geometry.computeTangents();
+
+            resolve(unwrappedProxyMesh);
+        });
     };
+    
+    const createHighlightMesh = (originalMesh: Mesh, color: ColorRepresentation = 0xff9800) => {
+        const highlightMesh = new Mesh();
+        highlightMesh.geometry = originalMesh.geometry.clone();
+        highlightMesh.geometry.drawRange.count = 0;
+        highlightMesh.material = new MeshBasicMaterial({
+            opacity: 0.5,
+            color: color,
+            depthWrite: false,
+            transparent: true,
+            
+        });
+        highlightMesh.renderOrder = 1;
+        
+        return highlightMesh;
+    }
 
     const disposeMesh = () => {
         unwrappedMesh?.traverse((obj) => {
@@ -76,7 +100,6 @@ export default function AnnotationController(props: AnnotationViewerProps) {
                 obj.material.dispose();
             }
         });
-        setUnwrappedMesh(null);
         highlightMesh?.geometry.disposeBoundsTree();
         highlightMesh?.geometry.dispose();
     }
@@ -88,44 +111,21 @@ export default function AnnotationController(props: AnnotationViewerProps) {
 
     const onCancel = () => {
         setShowConfirmation(false);
-        highlightIndices([]);
+        highlightIndices(unwrappedMesh, highlightMesh, []);
         setTempIndices([]);
-    }
-
-    const highlightIndices = (indices: number[]) => {
-        let newIndices: number[] = [];
-        for (let i = 0; i < indices.length; i++) {
-            const index = indices[i] * 3;
-            const a = index + 0;
-            const b = index + 1;
-            const c = index + 2;
-            newIndices.push(a, b, c);
-        }
-
-        const indexAttr = (unwrappedMesh?.children[0] as Mesh).geometry.index;
-        const newIndexAttr = highlightMesh.geometry.index;
-        // update the highlight mesh
-        for ( let i = 0, l = newIndices.length; i < l; i ++ ) {
-            const ix = indexAttr?.getX( newIndices[i] );
-            newIndexAttr?.setX( i, ix as number );
-        }
-
-        highlightMesh.geometry.drawRange.count = newIndices.length;
-        if (newIndexAttr)
-            newIndexAttr.needsUpdate = true;
     }
         
     const indicesSelectHandler = (indices: number[]) => {
         setTempIndices(indices);
-        highlightIndices(indices);
+        highlightIndices(unwrappedMesh, highlightMesh, indices);
         setShowConfirmation(true);
     }
 
     useEffect(() => {
-        unwrapMesh();
+        init();
         setShowConfirmation(false);
         return () => disposeMesh();
-    }, [proxyGeometry, proxyMaterial, unwrapAxis]);
+    }, [proxyGeometry, proxyMaterial]);
 
 
 	return (
@@ -134,13 +134,13 @@ export default function AnnotationController(props: AnnotationViewerProps) {
 				<CameraController />
                 <HoverIndex rate={0} handleHover={hoverIndexHandler} />
                 <LassoSelector
-                    mesh={unwrappedMesh?.children[0] as Mesh}
+                    mesh={unwrappedMesh as Mesh}
                     handleOnSelect={ (!showConfirmation) && archetype ? indicesSelectHandler : ()=>{}}
                 />
 				<ambientLight />
 				<color attach="background" args={['gray']} />
 				<pointLight position={[10, 10, 10]} />
-                {unwrappedMesh && <primitive object={unwrappedMesh}  />}
+                {groupRef.current && <primitive object={groupRef.current} />}
 			</Canvas>
             {showConfirmation && <Confirmation label={"Add Pattern?"} onConfirm={onConfirm} onCancel={onCancel} />}
 		</div>
---
+++ client/src/features/annotation/utils/highlightIndices.ts
@@ -0,0 +1,24 @@
+import { Mesh } from "three";
+
+export function highlightIndices(originalMesh: Mesh, meshToHighlight: Mesh, indices: number[]) {
+    let newIndices: number[] = [];
+    for (let i = 0; i < indices.length; i++) {
+        const index = indices[i] * 3;
+        const a = index + 0;
+        const b = index + 1;
+        const c = index + 2;
+        newIndices.push(a, b, c);
+    }
+
+    const indexAttr = originalMesh.geometry.index;
+    const newIndexAttr = meshToHighlight.geometry.index;
+    // update the highlight mesh
+    for ( let i = 0, l = newIndices.length; i < l; i ++ ) {
+        const ix = indexAttr?.getX( newIndices[i] );
+        newIndexAttr?.setX( i, ix as number );
+    }
+
+    meshToHighlight.geometry.drawRange.count = newIndices.length;
+    if (newIndexAttr)
+        newIndexAttr.needsUpdate = true;
+}
