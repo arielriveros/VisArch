21183f5a9adcc0ad4793eb1da8e74be7cbd954c3
Ariel <ariel.riveros@ing.uchile.cl>
2023-09-01 16:36:04 -0400

    fix (client): no disconnection on task change

--- client/src/features/annotation/containers/annotation/manager/AnnotationManager.tsx
+++ client/src/features/annotation/containers/annotation/manager/AnnotationManager.tsx
@@ -124,7 +124,7 @@ export default function AnnotationManager() {
         return () => {
             socket.current?.disconnect();
         }
-    }, [task]);
+    }, []);
 
     return (
         <div className='annotation-manager-container'>
